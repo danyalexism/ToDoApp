fbfd72b0e44ca86646624e6083e0a9b16622cdc3
Dany <danyaleximoru@gmail.com>
2024-08-26 13:38:09 -0500

    definir metodo init y mark def add_tag def TodoBook - corregido arreglo de errores

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,11 +1,10 @@
-# TODO: Add code here
 class Todo:
     def __init__(self, code_id: int, title: str, description: str, completed: bool, tags: list[str]):
-        self.code_id = code_id
-        self.title = title
-        self.description = description
-        self.completed = False
-        self.tags = []
+        self.code_id: int = code_id
+        self.title: str = title
+        self.description: str = description
+        self.completed: bool = False
+        self.tags: list[str] = []
     def mark_completed(self):
         self.completed = True
 
@@ -14,13 +13,13 @@ class Todo:
             self.tags.append(tag)
 
     # terminar
-    def __str__(self):
+    # def __str__(self):
 
 class TodoBook:
-    def __init__(self, todos: dict[1,Todo]):
+    def __init__(self, todos: dict[1, Todo]):
         self.todos = {}
 
-
+    # def add_todo(self):
 
 
 
