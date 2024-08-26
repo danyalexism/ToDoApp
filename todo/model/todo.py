a80193c64d58c253d99056799ac5f171737b9262
Dany <danyaleximoru@gmail.com>
2024-08-26 12:40:24 -0500

    definir metodo init y mark

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,10 @@
 # TODO: Add code here
+class Todo:
+    def __init__(self, code_id: int, title: str, description: str, completed: bool, tags: list[str]):
+        self.code_id = code_id
+        self.title = title
+        self.description = description
+        self.completed = False
+        self.tags = []
+    def mark_completed(self):
+        self.completed = True
